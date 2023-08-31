e5b04229bc86612cf3ece8fe8446c573bdc67c80
Mangirish <naikmangirish@gmail.com>
2023-09-01 02:46:10 +0530

    Added scene to build Organised folders Created dual sided shader to allow flipping Texture atlas for materials has been added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,5 +4,28 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    
+    #region SERIALISED_FIELDS
+    [SerializeField]
+    private Vector2Int _gridSize;
+    #endregion
+
+    #region PRIVATE_FIELDS
+    private float _visibilityDuration = 4;
+    private int _score = 0;
+    #endregion
+
+    public void NewGame()
+    {
+
+    }
+
+    public void Restart()
+    {
+
+    }
+
+    void CreateGrid()
+    {
+
+    }
 }
