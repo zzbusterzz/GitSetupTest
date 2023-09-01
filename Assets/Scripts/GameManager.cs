d6ebe151272909f6b4c09729d201e8cce1b887f1
Mangirish <naikmangirish@gmail.com>
2023-09-01 19:08:58 +0530

    Added basic unoptimised grid creator. Created Spawn system for cards without pooling support

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,19 +8,34 @@ public class GameManager : MonoBehaviour
     private Vector2Int _gridSize;
 
     [SerializeField]
-    private GameObject _flippingCardPrefab;
+    private Vector2 _gridDimensionsHalf;
+
+    [SerializeField]
+    private Card _flippingCardPrefab;
+
+    [SerializeField]
+    private Transform _gridStart;
     #endregion
 
     #region PRIVATE_FIELDS
     private float _visibilityDuration = 4;
     private int _score = 0;
+    private List<Vector3> _gridPosition;
+    private List<Card> _activeCards;
     #endregion
 
-    
+
+    private void Start()
+    {
+        _gridPosition = new List<Vector3>();
+        _activeCards = new List<Card>();
+        NewGame();
+    }
 
     public void NewGame()
     {
-
+        CreateGrid();
+        GenerateCards();
     }
 
     public void Restart()
@@ -31,8 +45,64 @@ public class GameManager : MonoBehaviour
 
     void CreateGrid()
     {
+        Vector3 gridStartVector = _gridStart.position + new Vector3(_gridDimensionsHalf.x, -_gridDimensionsHalf.y, 0);
+        float dimWidth = 2 * _gridDimensionsHalf.x;
+        float dimHeight = 2 * _gridDimensionsHalf.y;
 
+        for (int i = 0; i < _gridSize.x; i++)
+        {
+            for(int j = 0; j < _gridSize.y; j++)
+            {
+                _gridPosition.Add(new Vector3(
+                    gridStartVector.x + (i * dimWidth),
+                    gridStartVector.y - (j * dimHeight),
+                    0));
+            }
+        }
     }
 
-    
+    void GenerateCards()
+    {
+        int totalCards = _gridSize.x * _gridSize.y;
+
+        if( totalCards % 2 == 0 ) 
+        {
+            int uniqueCardsToGen = totalCards / 2;
+
+            Vector2Int _maxAllowedCards = new Vector2Int(13, 4);
+
+            HashSet<int> numbers = new HashSet<int>();
+
+            while(numbers.Count < uniqueCardsToGen) 
+            {
+                numbers.Add(Random.Range(0, 52));
+            }
+
+            foreach (int number in numbers)
+            {
+                Vector3 pos = GetGridPosition();
+                Card c = GetCardInstance(pos);
+                c.SetOffset(number % (_maxAllowedCards.x - 1), Mathf.FloorToInt(number / _maxAllowedCards.x));
+                _activeCards.Add(c);
+
+                pos = GetGridPosition();
+                c = GetCardInstance(pos);
+                c.SetOffset(number % (_maxAllowedCards.x - 1), Mathf.FloorToInt(number / _maxAllowedCards.x));
+                _activeCards.Add(c);
+            }
+        }
+    }
+
+    private Card GetCardInstance(Vector3 pos)
+    {
+        return GameObject.Instantiate(_flippingCardPrefab, pos, Quaternion.identity);
+    }
+
+    private Vector3 GetGridPosition()
+    {
+        int gridIndex = Random.Range(0, _gridPosition.Count);
+        Vector3 pos = _gridPosition[gridIndex];
+        _gridPosition.Remove(pos);
+        return pos;
+    }
 }
